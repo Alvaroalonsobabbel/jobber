07a80e4375a7c2118393d2a7fa5a95a899fe4faa
Alvaroalonsobabbel <aalonso@babbel.com>
2025-11-22 14:41:49 +0100

    Move test DB setup to db package for reuse

--- jobber/jobber_test.go
+++ jobber/jobber_test.go
@@ -1,22 +1,15 @@
 package jobber
 
 import (
-	"context"
 	"database/sql"
 	"errors"
 	"io"
 	"log/slog"
-	"path/filepath"
 	"slices"
 	"testing"
 	"time"
 
 	"github.com/Alvaroalonsobabbel/jobber/db"
-	"github.com/docker/go-connections/nat"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 type mockScraper struct{}
@@ -27,7 +20,7 @@ func (m *mockScraper) scrape(*db.Query) ([]db.CreateOfferParams, error) {
 
 func TestCreateQuery(t *testing.T) {
 	l := slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{}))
-	d, dbCloser := testDB(t)
+	d, dbCloser := db.NewTestDB(t)
 	defer dbCloser()
 	j, jCloser := newConfigurableJobber(l, d, &mockScraper{})
 	defer jCloser()
@@ -76,7 +69,7 @@ func TestCreateQuery(t *testing.T) {
 
 func TestListOffers(t *testing.T) {
 	l := slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{}))
-	d, dbCloser := testDB(t)
+	d, dbCloser := db.NewTestDB(t)
 	defer dbCloser()
 	j, jCloser := newConfigurableJobber(l, d, &mockScraper{})
 	defer jCloser()
@@ -124,75 +117,4 @@ func TestListOffers(t *testing.T) {
 			}
 		})
 	}
-}
-
-var seed = `
-INSERT INTO queries (keywords, location) VALUES
-('python', 'san francisco'),
-('data scientist', 'new york'),
-('golang', 'berlin');
-INSERT INTO offers (id, title, company, location, posted_at) VALUES
-('offer_001', 'Senior Python Developer', 'TechCorp Inc', 'San Francisco, CA', CURRENT_TIMESTAMP - INTERVAL '8 days'),
-('existing_offer', 'Junior Golang Dweeb', 'Späti GmbH', 'Berlin', CURRENT_TIMESTAMP);
-INSERT INTO query_offers (query_id, offer_id) VALUES
-(1, 'offer_001'),
-(3, 'existing_offer'),
-(1, 'existing_offer');
-`
-
-func testDB(t testing.TB) (*db.Queries, func()) {
-	t.Helper()
-	ctx := context.Background()
-
-	var (
-		dbImage          = "postgres:latest"
-		dbName           = "jobber"
-		dbPort  nat.Port = "5432/tcp"
-	)
-
-	postgresContainer, err := postgres.Run(ctx,
-		dbImage,
-		postgres.WithDatabase(dbName),
-		postgres.WithInitScripts(fetchMigrationFiles(t)...),
-		testcontainers.WithWaitStrategy(
-			wait.ForListeningPort(dbPort)),
-	)
-	if err != nil {
-		t.Fatalf("failed to start DB container: %s", err)
-	}
-
-	connStr, err := postgresContainer.ConnectionString(ctx, "sslmode=disable")
-	if err != nil {
-		t.Fatalf("failed to get container host: %s", err)
-	}
-
-	conn, err := pgxpool.New(ctx, connStr)
-	if err != nil {
-		t.Fatalf("unable to initialize db connection: %v", err)
-	}
-
-	if err := conn.Ping(ctx); err != nil {
-		t.Fatalf("unable to ping the DB: %v", err)
-	}
-
-	_, err = conn.Exec(ctx, seed)
-	if err != nil {
-		t.Fatalf("unable to seed DB: %v", err)
-	}
-
-	return db.New(conn), func() {
-		conn.Close()
-		if err := testcontainers.TerminateContainer(postgresContainer); err != nil {
-			t.Errorf("failed to terminate container: %s", err)
-		}
-	}
-}
-
-func fetchMigrationFiles(t testing.TB) []string {
-	t.Helper()
-	files, err := filepath.Glob("../db/migrations/*.up.sql")
-	if err != nil {
-		t.Fatalf("unable to read sql files: %v", err)
-	}
-	return files
 }
